a0439d5a21c63ece77cbceca5969d701594c8a3b
jaeeon99 <143310998+jaeeon99@users.noreply.github.com>
2024-12-09 16:10:48 +0900

    add: themeData

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        highlightColor: Colors.orange,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            foregroundColor: WidgetStatePropertyAll(Colors.white), // 버튼의 글자색
+            backgroundColor: WidgetStatePropertyAll(Colors.orange), // 버튼의 색상
+            minimumSize: WidgetStatePropertyAll(
+              Size.fromHeight(50),
+            ), // 버튼의 가로크기 인피니티, 세로 크기기만 50 설정
+            textStyle: WidgetStatePropertyAll(
+              TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            shape: WidgetStatePropertyAll(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8), // 버튼 모서리
+              ),
+            ),
+          ),
+        ),
+      ),
       home: WelcomePage(),
     );
   }
 }
-
--- lib/ui/pasges/welcome/welcome_page.dart
+++ lib/ui/pasges/welcome/welcome_page.dart
@@ -4,8 +4,35 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Text('WelcomePage'),
-    );
+        appBar: AppBar(),
+        body: SizedBox.expand(
+          //컬럼의 크기를 최대로
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/welcome.png',
+                height: 250,
+              ),
+              Text(
+                '당신 근처의 마켓',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                '동네라서 가능한 모든것\n 지금 내 동네를 선택하고 시작해보세요!',
+                // 역슬래시 (\n)은 줄 바꿈
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('시작하기'),
+              ),
+            ],
+          ),
+        ));
   }
 }
