73326435288fc59099844edc16c96ea370d61b6e
jaeeon99 <143310998+jaeeon99@users.noreply.github.com>
2024-12-09 18:54:15 +0900

    add: address_page

--- lib/ui/pasges/address_search/address_search_page.dart
+++ lib/ui/pasges/address_search/address_search_page.dart
@@ -1,11 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_maket_app/ui/pasges/join/widgets/join_page.dart';
 
 class AddressSearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Text('AddressSearchPage'),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: TextField(
+            onSubmitted: (value) {
+              //
+              print('onsubmitted $value');
+            },
+            decoration: InputDecoration(
+              hintText: '동명(읍,면)으로 검색 (ex, 서초동)',
+              contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+            ),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(height: 10),
+              SizedBox(
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('현재 위치로 찾기'),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (contex, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return JoinPage();
+                        }));
+                      },
+                      child: Container(
+                        height: 50,
+                        width: double.infinity,
+                        color: Colors.transparent,
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          '부산 광역시 동래구 온천동',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
