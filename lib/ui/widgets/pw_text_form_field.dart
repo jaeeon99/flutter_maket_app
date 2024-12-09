4be6f148c17da79a6bd8dfa5f640e7e0974f10f7
jaeeon99 <143310998+jaeeon99@users.noreply.github.com>
2024-12-09 16:36:45 +0900

    add: pw_text_form_field

---
+++ lib/ui/widgets/pw_text_form_field.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_maket_app/core/validator_util.dart';
+
+class PwTextFormField extends StatelessWidget {
+  PwTextFormField({
+    required this.controller,
+  });
+  TextEditingController controller;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(hintText: '비밀번호를 입력해 주세요'),
+      obscureText: true, // 비밀번호를 안보이게 해주는 역할
+      validator: ValidatorUtil.ValidatorId,
+    );
+  }
+}
